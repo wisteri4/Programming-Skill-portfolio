ce45e04bce9849c42b63262625cb39904d6bde60
Izabel <165130557+wisteri4@users.noreply.github.com>
2024-10-24 10:58:41 +0400

    Add files via upload

---
+++ ex1_codingiscool.py
@@ -0,0 +1,11 @@
+'''EX-1 Coding is Cool'''
+
+word1 = "Coding" 
+#declare 'coding' in double quotation to word1 variable
+word2 = "is"
+#declare 'is' to the variable word2
+word3 = "Cool"
+#declare 'cool' to variable word3
+
+print(word1, word2, word3) 
+#print variables in the order word1, word2 and word 3 to generate Coding is Cool
