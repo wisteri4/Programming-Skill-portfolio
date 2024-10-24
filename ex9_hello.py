a5f31c95b15aa2f09779aafbcd9bec3aebd34cca
Izabel <165130557+wisteri4@users.noreply.github.com>
2024-10-24 11:08:53 +0400

    Add files via upload

---
+++ ex9_hello.py
@@ -0,0 +1,10 @@
+'''EX 9 - Hello '''
+
+def hello(): 
+    print("hello") #this def prints hello
+    
+def main():
+    return hello() #calls hello function 
+    
+if __name__ == "__main__":
+    main() #if condition is true call main function
