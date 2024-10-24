1cc17ebe4a3f1bd2e753aed46c3211a6a49b5b2b
Izabel <165130557+wisteri4@users.noreply.github.com>
2024-10-24 11:02:39 +0400

    Add files via upload

---
+++ ex4_primitivequiz_w_advance_req.py
@@ -0,0 +1,99 @@
+'''EX 4 - Primitive Quiz'''
+
+capital = input("What is the Capital of France? ")
+#input from the user the word needed and store in the variable 'capital' 
+
+if capital == "Paris": #if user input value is true 
+    print("you are correct!") #then print correct 
+elif capital =="paris": #else if value is true and all letters lowercase 
+    print("you are correct!") #print you are correct statement 
+elif capital =="PaRis": #else if value has p and r in capital 
+    print("you are correct!") #print correct statement 
+else: #else value is false
+    print("That is wrong") #print a message saying the answer is wrong
+
+'''====10 European countries===='''
+
+cap1 = input("What is the Capital of Romania? ") # user input the word and store in the variable 'cap1'
+if cap1 == "Bucharest": #if user input value is true 
+    print("you are correct!") #print you are correct statement 
+elif cap1 == "bucharest": #else if value is true and all letters lowercase 
+    print("you are correct!") #print correct statement 
+else:
+    print("That is wrong, try again next time") #print statement indicating value is wrong
+
+cap2 = input("What is the Capital of Germany? ")
+if cap2 == "Berlin": #if user input value is true 
+    print("you are correct!") #print you are correct statement 
+elif cap2 == "berlin": #else if value is true and all letters lowercase 
+    print("you are correct!") 
+else:
+    print("That is wrong, try again next time") #print statement indicating value is wrong
+
+cap3 = input("What is the Capital of Italy? ")
+if cap3 == "Rome": #if user input value is true 
+    print("you are correct!") #print you are correct statement 
+elif cap3 == "rome": #else if value is true and all letters lowercase 
+    print("you are correct!") #print that user is correct statement
+else:
+    print("That is wrong, try again next time") #print statement indicating value is wrong
+
+cap4 = input("What is the Capital of Georgia? ")
+if cap4 == "Tbilisi": #if user input value is true 
+    print("you are correct!") #print that user is correct statement
+elif cap4 == "tbilisi": #else if value is true and all letters lowercase 
+    print("you are correct!") 
+else:
+    print("That is wrong, try again next time") #print statement indicating value is wrong
+    
+cap5 = input("What is the Capital of Ukraine? ")
+if cap5 == "Kiev": #if user input value is true 
+    print("you are correct!") #print you are correct statement 
+elif cap5 == "kiev": #else if value is true and all letters lowercase 
+    print("you are correct!") #print that user is correct statement
+else:
+    print("That is wrong, try again next time") #print statement indicating value is wrong
+
+cap6 = input("What is the Capital of Czech Republic? ")
+if cap6 == "Prague": #if user input value is true 
+    print("you are correct!") #print you are correct statement 
+elif cap6 == "prague": #else if value is true and all letters lowercase 
+    print("you are correct!") #print that user is correct statement
+else:
+    print("That is wrong, try again next time")#print statement indicating value is wrong
+
+cap7 = input("What is the Capital of Austria? ")
+if cap7 == "Vienna": #if user input value is true 
+    print("you are correct!") #print you are correct statement 
+elif cap7 == "vienna": #else if value is true and all letters lowercase 
+    print("you are correct!") #print that user is correct statement
+else:
+    print("That is wrong, try again next time") #print statement indicating value is wrong
+
+cap8 = input("What is the Capital of Norway? ")
+if cap8 == "Oslo": #if user input value is true 
+    print("you are correct!") #print you are correct statement 
+elif cap8 == "oslo": #else if value is true and all letters lowercase 
+    print("you are correct!") #print that user is correct statement
+else:
+    print("That is wrong, try again next time") #print statement indicating value is wrong
+  
+cap9 = input("What is the Capital of Poland? ")
+if cap9 == "Warsaw": #if user input value is true 
+    print("you are correct!") #print you are correct statement 
+elif cap9 == "warsaw": #else if value is true and all letters lowercase 
+    print("you are correct!") #print that user is correct statement
+else:
+    print("That is wrong, try again next time") #print statement indicating value is wrong
+
+cap10 = input("What is the Capital of Russia? ")
+if cap10 == "Moscow": #if user input value is true 
+    print("you are correct!") #print you are correct statement 
+elif cap10 == "moscow": #else if value is true and all letters lowercase 
+    print("you are correct!") #print that user is correct statement
+else:
+    print("That is wrong, try again next time") #print statement indicating value is wrong
+
+    
+
+
