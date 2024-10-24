58e6711a679d2a56c0d58843d61013c930f754da
Izabel <165130557+wisteri4@users.noreply.github.com>
2024-10-24 11:07:35 +0400

    Add files via upload

---
+++ ex7_somecounting.py
@@ -0,0 +1,18 @@
+'''EX 7 - Some counting'''
+
+for a in range (0,51): #for loop to count from 0 to 50 in increments of 1 
+    print(a) #print the variable 
+
+for b in range (51,0,-1): #50 to 0 in decrements of 1
+    print(b) #print variable 
+
+for c in range (30,51): #30 to 50 in increments of 1 
+    print(c) # print variable 
+
+for d in range (50, 10, -2): #50 to 10 in decrements of 2
+    print(d) #print variable 
+
+for e in range(100,200, 5): #100 t0 200 in increments of 5
+    print(e) #print variable 
+
+#all in one single project without sections 
