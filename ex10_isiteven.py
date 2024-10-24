04b6ac979ffa415ef7122d198a112dea6aba058f
Izabel <165130557+wisteri4@users.noreply.github.com>
2024-10-24 11:09:29 +0400

    Add files via upload

---
+++ ex10_isiteven.py
@@ -0,0 +1,9 @@
+'''EX 10 - is it even? '''
+
+oddeven= int(input("Please enter a random number: ")) #user input number in integer
+
+if oddeven % 2 == 0: #if the number remainder divided by 2 is 0
+    print ("number given is even") #then the number is even
+else:#else when the given number has a remainder
+    print ("number given is odd") #then the number is odd 
+    
