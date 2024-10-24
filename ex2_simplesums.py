82326a42a6d218db756550b6dda57dffbbc9d916
Izabel <165130557+wisteri4@users.noreply.github.com>
2024-10-24 10:59:50 +0400

    Add files via upload

---
+++ ex2_simplesums.py
@@ -0,0 +1,12 @@
+''' EX-2 Simple Sums'''
+
+# declare the integer in variables firstnum and secondnum
+firstnum = 8
+secondnum = 10
+
+# total stores the sum of the two variables
+total = firstnum + secondnum
+
+#print using f-string with the variable total as the placeholder 
+print(f"The sum of the first and second number is {total}")
+
